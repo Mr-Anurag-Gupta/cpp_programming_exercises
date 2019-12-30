15dd4f54fee1621bb3702450daa79d899dbb94bc
Anurag Gupta <anuragjigupta17@gmail.com>
2019-12-30 22:49:48 +0530

    Solution for problem 3.9

---
+++ 3. Tokens, Expressions and Control Structures/ex_3.10.cpp
@@ -0,0 +1,22 @@
+/*
+ * Problem:
+ *  Write a program to read the names of users and
+ *  number of units consumed and print out the charges
+ *  with names.
+*/
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <cmath>
+
+void readRecord()
+
+int main()
+{
+    map<std::string, int> db;
+
+
+    return EXIT_SUCCESS;
+}
