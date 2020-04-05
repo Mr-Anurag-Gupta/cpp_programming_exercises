3d4652dce4c58f0dfce43c0aa1e69f8dfe455d91
Anurag Gupta <anuragjigupta17@gmail.com>
2020-04-05 19:31:48 +0530

    Complete solutions for problems 7.2, 7.3 and 7.5

--- 7. Operator Overloading and Type Conversions/ex_7.3.cpp
+++ 7. Operator Overloading and Type Conversions/ex_7.3.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <cassert>
+#include <exception>
 
 class MAT
 {
@@ -8,37 +9,65 @@ private:
     int rows;
     int cols;
     int **mat;
+    void allocateSpace();
 
 public:
-    MAT() : rows(0), cols(0), mat(nullptr) {}
-    MAT(int rows, int cols);
-    MAT(int rows, int cols, int **arr) : rows(rows), cols(cols) { initMatrix(arr); }
+    MAT() : rows(0), cols(0), mat(nullptr){};
+    MAT(int rows, int cols) : rows(rows), cols(cols){};
+    MAT(int rows, int cols, int *arr);
 
     // Basic Operations.
     int getElement(int row, int col) { return mat[row][col]; }
     void putElement(int row, int col, int value) { mat[row][col] = value; }
-    void initMatrix(int **);
+    void initMatrix(int *);
     void display();
 
-    // Overloaded operators.
+    // Overloaded operators. (<object> <(+|-|*|/|)> <value>)
+    void operator*(int);
+    void operator+(int);
+    void operator/(int);
+    void operator-(int);
+
+    // Returns the resultant matrix after the operation is performed.
+    MAT operator+(const MAT &);
+    MAT operator*(const MAT &);
+    MAT operator/(const MAT &);
+    MAT operator-(const MAT &);
+
+    friend void operator*(int, MAT &);
+    friend void operator+(int, MAT &);
+    friend void operator/(int, MAT &);
+    friend void operator-(int, MAT &);
 };
 
-void MAT::initMatrix(int **arr)
+MAT::MAT(int rows, int cols, int *arr)
+{
+    this->rows = rows;
+    this->cols = cols;
+    allocateSpace();
+    initMatrix(arr);
+}
+
+void MAT::allocateSpace()
 {
     // Allocate space
-    mat = new int *[rows];
-    if (mat == nullptr)
+    try
     {
-        std::cerr << "Allocation failed." << std::endl;
-        exit(0);
+        mat = new int *[rows];
+        for (auto i = 0; i < rows; i++)
+            mat[i] = new int[cols];
     }
+    catch (std::bad_alloc ex)
+    {
+        std::cerr << "bad_alloc: " << ex.what() << std::endl;
+    }
+}
 
-    for (auto i = 0; i < rows; i++)
-        mat[i] = new int[cols];
-
+void MAT::initMatrix(int *arr)
+{
     for (auto i = 0; i < rows; i++)
         for (auto j = 0; j < cols; j++)
-            mat[rows][cols] = *(*(arr + i) + j);
+            mat[i][j] = *(arr + i * cols + j);
 }
 
 void MAT::display()
@@ -46,21 +75,193 @@ void MAT::display()
     for (auto i = 0; i < rows; i++)
     {
         for (auto j = 0; j < cols; j++)
-            std::cout << mat[rows][cols] << " ";
+            std::cout << mat[i][j] << " ";
         std::cout << std::endl;
     }
+}
+
+void MAT::operator*(int value)
+{
+    for (auto i = 0; i < rows; i++)
+        for (auto j = 0; j < cols; j++)
+            mat[i][j] *= value;
+}
+
+void MAT::operator+(int value)
+{
+    for (auto i = 0; i < rows; i++)
+        for (auto j = 0; j < cols; j++)
+            mat[i][j] += value;
+}
+
+void MAT::operator/(int value)
+{
+    for (auto i = 0; i < rows; i++)
+        for (auto j = 0; j < cols; j++)
+            mat[i][j] /= value;
+}
+
+void MAT::operator-(int value)
+{
+    for (auto i = 0; i < rows; i++)
+        for (auto j = 0; j < cols; j++)
+            mat[i][j] -= value;
+}
+
+void operator*(int value, MAT &m)
+{
+    for (auto i = 0; i < m.rows; i++)
+        for (auto j = 0; j < m.cols; j++)
+            m.mat[i][j] *= value;
+}
+
+void operator+(int value, MAT &m)
+{
+    for (auto i = 0; i < m.rows; i++)
+        for (auto j = 0; j < m.cols; j++)
+            m.mat[i][j] += value;
+}
+
+void operator/(int value, MAT &m)
+{
+    for (auto i = 0; i < m.rows; i++)
+        for (auto j = 0; j < m.cols; j++)
+            m.mat[i][j] /= value;
+}
+
+void operator-(int value, MAT &m)
+{
+    for (auto i = 0; i < m.rows; i++)
+        for (auto j = 0; j < m.cols; j++)
+            m.mat[i][j] -= value;
+}
+
+MAT MAT::operator+(const MAT &m1)
+{
+    assert(rows == m1.rows);
+    assert(cols == m1.cols);
+
+    MAT m3(rows, cols);
+    m3.allocateSpace();
+
+    for (auto i = 0; i < m3.rows; i++)
+        for (auto j = 0; j < m3.cols; j++)
+            m3.mat[i][j] = mat[i][j] + m1.mat[i][j];
+
+    return m3;
+}
+
+MAT MAT::operator-(const MAT &m1)
+{
+    assert(rows == m1.rows);
+    assert(cols == m1.cols);
+
+    MAT m3(rows, cols);
+    m3.allocateSpace();
+
+    for (auto i = 0; i < m3.rows; i++)
+        for (auto j = 0; j < m3.cols; j++)
+            m3.mat[i][i] = mat[i][j] - m1.mat[i][j];
+
+    return m3;
+}
+
+MAT MAT::operator/(const MAT &m1)
+{
+    assert(rows == m1.rows);
+    assert(cols == m1.cols);
+
+    MAT m3(rows, cols);
+    m3.allocateSpace();
+
+    for (auto i = 0; i < m3.rows; i++)
+        for (auto j = 0; j < m3.cols; j++)
+            m3.mat[i][i] = mat[i][j] / m1.mat[i][j];
+
+    return m3;
+}
+
+MAT MAT::operator*(const MAT &m1)
+{
+    // For matrix multiplications, (col of one matrix)
+    // should be == (row of other matrix).
+    assert(cols == m1.rows);
+
+    MAT m3(rows, m1.cols);
+    m3.allocateSpace();
+
+    int sum;
+    for (auto i = 0; i < rows; i++)
+    {
+        sum = 0;
+        for (auto j = 0; j < m1.cols; j++)
+        {
+            for (auto k = 0; k < m1.rows; k++)
+                sum += mat[i][k] * m1.mat[k][j];
+            m3.mat[i][j] = sum;
+        }
+    }
+
+    return m3;
 }
 
 int main()
 {
     const int rows = 2, cols = 3;
-    MAT m(rows, cols);
-
-    int arr[rows][cols] = {
+    int m1_arr[rows][cols] = {
         {1, 2, 3},
         {4, 5, 6}};
-    m.initMatrix((int **)arr);
-    m.display();
+    int m2_arr[cols][rows] = {
+        {1, 2},
+        {3, 4},
+        {5, 6}};
+
+    MAT m1(rows, cols, (int *)m1_arr);
+    MAT m2(cols, rows, (int *)m2_arr);
+    MAT m3;
+    MAT m4(rows, cols, (int *)m1_arr);
+
+    std::cout << "\nm1:" << std::endl;
+    m1.display();
+
+    std::cout << "\nm2:" << std::endl;
+    m2.display();
+
+    std::cout << "\nm4:" << std::endl;
+    m4.display();
+
+    // Test cases.
+    std::cout << "\n(*):" << std::endl;
+    m1 * 2;
+    m1.display();
+
+    std::cout << "\n(/):" << std::endl;
+    m1 / 2;
+    m1.display();
+
+    std::cout << "\n(-):" << std::endl;
+    m1 - 1;
+    m1.display();
+
+    std::cout << "\n(+):" << std::endl;
+    m1 + 1;
+    m1.display();
+
+    std::cout << "\n(m1 * m2):" << std::endl;
+    m3 = m1 * m2;
+    m3.display();
+
+    std::cout << "\n(m1 + m4):" << std::endl;
+    m3 = m1 + m4;
+    m3.display();
+
+    std::cout << "\n(m1 - m4):" << std::endl;
+    m3 = m1 - m4;
+    m3.display();
+
+    std::cout << "\n(m1 / m4):" << std::endl;
+    m3 = m1 / m4;
+    m3.display();
 
     return EXIT_SUCCESS;
 }
