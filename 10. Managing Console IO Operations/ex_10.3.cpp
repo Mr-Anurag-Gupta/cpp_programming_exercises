30f59cb89ebb2bc3c126a9069e86e245cb5b9705
Anurag Gupta (agupta) <Anurag.Gupta@quest.com>
2020-06-14 18:46:35 +0530

    Made changes in ex_10.3.cpp

--- 10. Managing Console IO Operations/ex_10.3.cpp
+++ 10. Managing Console IO Operations/ex_10.3.cpp
@@ -1,40 +1,41 @@
 #include <iostream>
 #include <iomanip>
 
-void calc_whitespaces_letters_lines();
+const int LINEFEED = 0x0a;
+
+void calc_whilespaces_letters_lines();
+bool isnewline(unsigned int);
 
 int main(int argc, char **argv)
 {
-    std::cout << "\nFIND NUMBER OF CHARACTERS, WHITESPACES & LINES";
-    std::cout << "\nINPUT TEXT (MARK THE END WITH '#'):\n";
+    std::cout << "\nFIND NUMBER OF CHARACTERS, WHITESPACES & LINES:\n";
+    std::cout << "INPUT TEXT (MARK THE END WITH '#'):\n\n";
 
-    calc_whitespaces_letters_lines();
+    calc_whilespaces_letters_lines();
 
     std::cin.get();
     return EXIT_SUCCESS;
 }
 
-void calc_whitespaces_letters_lines()
+void calc_whilespaces_letters_lines()
 {
     char ch;
-    int num_of_lines, num_of_whitespaces, num_of_characters;
+    int num_of_whitespace, num_of_lines, num_of_characters;
 
-    num_of_lines = num_of_whitespaces = num_of_characters = 0;
+    num_of_whitespace = num_of_lines = num_of_characters = 0;
 
     do
     {
-        // scan the character from the stdin.
         std::cin.get(ch);
 
-        // logic
         if (ch != '#')
         {
             if (isalnum(ch) || ispunct(ch))
                 num_of_characters++;
-            else if (ch == '\n')
+            else if (isnewline(ch))
                 num_of_lines++;
             else if (isspace(ch))
-                num_of_whitespaces++;
+                num_of_whitespace++;
         }
 
     } while (ch != '#');
@@ -43,17 +44,22 @@ void calc_whitespaces_letters_lines()
     std::cout.setf(std::ios::left, std::ios::adjustfield);
     std::cout << std::setw(28) << "Number of characters:";
     std::cout.setf(std::ios::right, std::ios::adjustfield);
-    std::cout << std::setw(5) << num_of_characters;
+    std::cout << std::setw(5) << num_of_characters << "\n";
 
-    std::cout << "\n";
     std::cout.setf(std::ios::left, std::ios::adjustfield);
-    std::cout << std::setw(28) << "Number of whitespaces:";
+    std::cout << std::setw(28) << "Number of whitespace:";
     std::cout.setf(std::ios::right, std::ios::adjustfield);
-    std::cout << std::setw(5) << num_of_whitespaces;
+    std::cout << std::setw(5) << num_of_whitespace << "\n";
 
-    std::cout << "\n";
     std::cout.setf(std::ios::left, std::ios::adjustfield);
     std::cout << std::setw(28) << "Number of lines:";
     std::cout.setf(std::ios::right, std::ios::adjustfield);
     std::cout << std::setw(5) << num_of_lines << "\n";
+}
+
+bool isnewline(unsigned int ch)
+{
+    if (ch == LINEFEED)
+        return true;
+    return false;
 }
